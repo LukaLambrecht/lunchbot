b590357a5c164aa05c6757f658f324ffe826adb2
Luka Lambrecht <llambrec@cern.ch>
2024-07-30 16:50:45 +0200

    add automatic birthday notifications

--- lunchbot.py
+++ lunchbot.py
@@ -3,12 +3,53 @@
 # References:
 # - https://developers.mattermost.com/integrate/webhooks/incoming/?utm_source=mattermost&utm_medium=in-product&utm_content=installed_incoming_webhooks&uid=d5p7qup7off9jy48u5qbek68uc&sid=1q5ehw3axtgk9ex8e9i8nez95o
 
+
 import os
 import json
 import argparse
 import requests
 
 from URL import lunchboturl
+
+
+def send_lunchbot_message(message=None, channel=None, verbose=False):
+    
+    # fixed settings
+    headers = {'Content-Type': 'application/json'}
+    icon_emoji = ':sandwich:'
+
+    # parse the message
+    text = ''
+    if message is None: text = 'Lunchtime!'
+    elif message.endswith('.txt'):
+        if os.path.exists(message):
+            with open(message, 'r') as f:
+                text = f.readlines()
+            text = ''.join(text)
+            text = text.strip(' \t\n')
+        else:
+            msg = 'ERROR: message file {} not found.'.format(message)
+            raise Exception(msg)
+    else: text = message
+    if verbose:
+        print('Found following message to send:')
+        print(text)
+
+    # make the json payload
+    payload = {}
+    payload['text'] = text
+    payload['icon_emoji'] = icon_emoji
+    if channel is not None: payload['channel'] = channel
+    if verbose:
+        print('Constructed following payload:')
+        print(payload)
+
+    # make the POST request
+    response = requests.post(lunchboturl, headers=headers, json=payload)
+    if verbose:
+        print('POST request returned the following response:')
+        print(response.text)
+
 
 if __name__=='__main__':
 
@@ -18,33 +59,9 @@ if __name__=='__main__':
     parser.add_argument('-c', '--channel', default=None)
     args = parser.parse_args()
 
-    # fixed settings
-    headers = {'Content-Type': 'application/json'}
-    icon_emoji = ':sandwich:'
-
-    # parse the message
-    text = ''
-    if args.message is None: text = 'Lunchtime!'
-    elif args.message.endswith('.txt'):
-        if os.path.exists(args.message):
-            with open(args.message, 'r') as f:
-                text = f.readlines()
-            text = ''.join(text)
-            text = text.strip(' \t\n')
-        else:
-            msg = 'ERROR: message file {} not found.'.format(args.message)
-            raise Exception(msg)
-    else: text = args.message
-
-    # make the json payload
-    payload = {}
-    payload['text'] = text
-    payload['icon_emoji'] = icon_emoji
-    if args.channel is not None: payload['channel'] = args.channel
-    print('Constructed follwing payload:')
-    print(payload)
-
-    # make the POST request
-    response = requests.post(lunchboturl, headers=headers, json=payload)
-    print('POST request returned the following response:')
-    print(response.text)
+    # send the message
+    send_lunchbot_message(
+            message=message,
+            channel=channel,
+            verbose=True
+    )
---
+++ messages/birthdays/check_birthdays.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+
+import os
+import sys
+import json
+import random
+import datetime
+import argparse
+
+
+sys.path.append(os.path.abspath(os.path.join(__file__,'../../..')))
+from lunchbot import send_lunchbot_message
+
+
+def get_cake_urls():
+    import requests
+    from bs4 import BeautifulSoup
+    baseurl = 'https://www.bbcgoodfood.com/recipes/collection/birthday-cake-recipes'
+    links = []
+    for page in [1,2,3]:
+        source = requests.get(baseurl+'?page={}'.format(page))
+        soup = BeautifulSoup(source.content, 'lxml')
+        links_raw = [str(el.get('href')) for el in soup.find_all('a')]
+        for link in links_raw:
+            if link.startswith('/'): link = 'https://www.bbcgoodfood.com/recipes'+link
+            if not link.startswith('https://www.bbcgoodfood.com/recipes/'): continue
+            if not 'cake' in link: continue
+            if 'search' in link: continue
+            if 'collection' in link: continue
+            if link in links: continue
+            links.append(link)
+    return links
+
+
+if __name__=='__main__':
+
+    # command line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--inputfile', default='birthdays.json')
+    parser.add_argument('-c', '--channel', default=None)
+    args = parser.parse_args()
+
+    # read input file
+    with open(args.inputfile, 'r') as f:
+        data = json.load(f)
+    print('Read following birthdays from input file:')
+    for name, date in sorted(data.items()): print('  - {}: {}'.format(name, date))
+
+    # convert dates to python date objects (datetime module)
+    # todo: current implementation using datetime.today().year
+    #       will not work for early warning messages
+    #       for birthdays just after new year,
+    #       need to somehow use the next year instead.
+    dates = {}
+    for name, datestr in sorted(data.items()):
+        try:
+            day = int(datestr.split('/')[0])
+            month = int(datestr.split('/')[1])
+            date = datetime.date(datetime.date.today().year, month, day)
+            dates[name] = date
+        except:
+            print('WARNING: did not find valid date for {}.'.format(name))
+
+    # define messages for today
+    messages = []
+    cake_urls = get_cake_urls()
+    for name, date in sorted(dates.items()):
+        # first warning, a few days in advance
+        if (date - datetime.timedelta(days=3)) == datetime.date.today():
+            m = "Hi @all, please be reminded that @{}'s birthday is coming soon".format(name)
+            m += " (on {}/{})!".format(date.day, date.month)
+            m += " @{}, please prepare a birthday cake for your dear colleagues".format(name)
+            m += " (for example this one: {})".format(random.choice(cake_urls))
+            messages.append(m)
+        # on the morning of the day itself
+        if date == datetime.date.today():
+            m = "Hi @all, final reminder of @{}'s birthday today!".format(name)
+            messages.append(m)
+    print('Found following birthday messages to be sent today:')
+    for message in messages: print('  - {}'.format(message))
+
+    # exit if no messages to display
+    if len(messages)==0:
+        print('No birthday messages to send today, exiting.')
+        sys.exit()
+
+    # send the messages
+    for message in messages:
+        send_lunchbot_message(message=message, channel=args.channel, verbose=True)
